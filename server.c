0d8561a8839611d6f22d568d461841d69d13495a
nProductionz <nProductionz@users.noreply.github.com>
2021-11-09 20:19:26 +0100

    Server.c accetta client

--- server.c
+++ server.c
@@ -6,6 +6,41 @@
 
 #define MAX_CLIENTS 20
 
+// Struttura del client
+
+typedef struct {
+    struct sockaddr_in address;
+    int sockfd;
+    int uid;
+    char username[36];
+} client_t;
+
+client_t *clients[MAX_CLIENTS];
+
+
+static _Atomic unsigned int client_counter = 0;
+void *handle_client(void *arg) {
+    char buff_output[BUFFER_SZ];
+    char username[36];
+    int deadEnd_flag = 0;     // this is a reference
+
+    client_counter++;
+    client_t *client = (client_t *)arg;
+
+    // Assegnazione Username
+    if(recv(client->sockfd, name, 36, 0) <= 0 || strlen(username) < 2 || strlen(username) >= 36-1) {
+       printf("Didn't enter the username.\nAborting...\n");
+       deadEnd_flag = 1; 
+    } else {
+        strcpy(client->username, username);
+        sprintf(buff_out, "%s has joined the chatroom\n", client->username);
+        printf("%s", buff_out);    
+        //
+        }
+}
+
+
+
 int main(int argc, char **argv){
-    
+
 }
