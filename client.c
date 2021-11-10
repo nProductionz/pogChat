b94dd6b1509478ecc2e61a4e619c5457e51938bb
nProductionz <75636093+nProductionz@users.noreply.github.com>
2021-11-10 20:58:35 +0100

    Iniziato il main client.c

--- client.c
+++ client.c
@@ -3,7 +3,29 @@
 int sockfd = 0;
 char username[36];
 
-int main(int argc, char **argv) {
-    
+void catch_ctrl_c_exit(int sig) {
+    deadEnd_flag = 1;
 }
 
+int main(int argc, char **argv) {
+    if(argc != 2){
+        printf("Porta in uso: %s <port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }   
+
+    char *ip = "127.0.0.1";
+    int port = atoi(argv[1]);
+
+    signal(SIGINT, catch_ctrl_c_exit); // Per terminare il processo con CTRL + C
+
+    printf("Inserire l'username: ");
+    fgets(username, 36, stdin);
+    //
+    if(strlen(name) > 36 || strlen(username) < 2) {
+        printf("Non fare scherzi e rispetta le regole col nome.\n");
+        return EXIT_FAILURE;
+    }
+
+    
+
+}
--- client.h
+++ client.h
@@ -4,3 +4,11 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+// variabili globali
+
+#define LENGTH 2048
