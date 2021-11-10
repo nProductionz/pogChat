3b237ac5d0f3e29b5f75c150802b87d163d16dbf
nProductionz <75636093+nProductionz@users.noreply.github.com>
2021-11-10 20:41:49 +0100

    created Client.h

--- client.c
+++ client.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/socket.h>
+#include "client.h"
 
 int sockfd = 0;
 char username[36];
