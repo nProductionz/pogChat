15bfdf373b156cb73e3ba8e40f5287ecda56b7f0
nProductionz <75636093+nProductionz@users.noreply.github.com>
2021-11-10 19:42:23 +0100

    Added listen and bind in main server

--- server.c
+++ server.c
@@ -1,14 +1,5 @@
 #include "server.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-
-#define MAX_CLIENTS 20
-#define BUFFER_SIZE 2048
-
 // Struttura del client
 
 typedef struct {
@@ -103,5 +94,52 @@ void *handle_client(void *arg) {
 
 
 int main(int argc, char **argv){
+    if(argc != 2){
+        printf("Porta in uso: %s", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    char *ip = "127.0.0.1";
+    int port = atoi(argv[1]);
+    int option = 1;
+    int listenfd = 0, connfd = 0;
+
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+    pthread_t tid;
+
+    // Setting socket
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(port);
+
+    // Ignore Pipe Signal
+    signal(SIGPIPE, SIG_IGN);
+    if(setsockopt(listenfd, SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),(char*)&option, sizeof(option)) < 0) {
+        perror("ERROR: setsockport failed");
+        return EXIT_FAILURE;
+    }
+
+    // Bind
+    if(bind(listenfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+        perror("ERROR: Socket listening failed");
+        return EXIT_FAILURE;
+    }
+
+    // Listen
+    if(listen(listenfd,10) < 0) {
+        perror("ERROR: Socket listening failed");
+        return EXIT_FAILURE;
+    }
+
+    printf(" +++ BENVENUTO NELLA CHATROOM +++\nsperiamo che sta cosa funzioni...\n");
+    while(1) {
+        socklen_t clientlen = sizeof(client_addr);
+        connfd = accept(listenfd, (struct sockaddr*)&client_addr, &clientlen);
+
+        // controllo se son stati accettati il massimo numero di client
+    }
+
 
 }
