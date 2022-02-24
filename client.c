bbe1903598908fcda3b5446f9b379e06217e9b63
nProductionz <marco.evans.12@gmail.com>
2022-02-24 17:42:31 +0100

    Removed useless things client

--- client.c
+++ client.c
@@ -9,59 +9,6 @@ void catch_ctrl_c_exit(int sig) {
     deadEnd_flag = 1;
 }
 
-// trim per ripulire la stringa nel caso di andate a capo
-void str_trim (char* arr, int length){
-    int i;
-    for( i = 0; i < length; i++) {
-        if(arr[i] == '\n'){
-            arr[i] = '\0';
-            break;
-        }
-    }
-}
-
-// per printare il nome prima del messaggio
-void str_overwrite_stdout() {
-    printf("%s", "> ");
-    fflush(stdout);
-}
-
-void send_msg_handler() {
-    char msg[LENGTH] = {};
-    char buffer[LENGTH + 34] = {};
-    
-    while(1) {
-        str_overwrite_stdout();
-        fgets(msg, LENGTH, stdin);
-        str_trim(msg, LENGTH);
-
-        if (strcmp(msg, "exit") == 0) {
-            break;  // uscire dalla chat
-        }   else    {
-            sprintf(buffer, "%s: %s\n", username, msg);
-            send(sockfd, buffer, strlen(buffer), 0);
-        }
-        bzero(msg, LENGTH);
-        bzero(buffer, LENGTH + 32);
-    }
-    catch_ctrl_c_exit(2);
-}
-
-void recv_msg_handler(){
-    char message[LENGTH] = {};
-    while(1) {
-        int receive = recv(sockfd, message, LENGTH, 0);
-        if(receive > 0) {
-            printf("%s", message);
-            str_overwrite_stdout();
-        } else if (receive == 0) {
-            break;   // se non riceve, chiudere il ciclo di ricezione
-        } else {
-            // -1
-        }
-        memset(message, 0, sizeof(message));    // azzera il messaggio
-    }
-}
 
 int main(int argc, char **argv) {
     if(argc != 2){
@@ -91,35 +38,11 @@ int main(int argc, char **argv) {
     server_addr.sin_addr.s_addr = inet_addr(ip);
     server_addr.sin_port = htons(port);
 
-    // Connessione col server
-    int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if(err == -1) {
-        printf("ERROR: connect\n");
-        return EXIT_FAILURE;
-    }    
+    
+    socklen_t addr_size;
 
-    // Inviare l'username al server
-    send(sockfd, username, 32, 0);
-    // sperando che tutto funzioni...
-    printf("+++ BENVENUTO NELLA POGCHAT +++\n");
-    // Creare un thread per inviare e gestire i messaggi
-    pthread_t send_msg_thread;
-    if(pthread_create(&send_msg_thread, NULL, (void *)send_msg_handler, NULL) != 0){
-        // implementare la funzione per ricevere messaggi
-        printf("ERROR: creazione del thread\n");
-        return EXIT_FAILURE;
-    }
-    pthread_t recv_msg_thread;
-    if(pthread_create(&recv_msg_thread, NULL, (void *)recv_msg_handler, NULL) != 0) {
-        printf("ERROR: creazione pthread del recv >\n");
-        return EXIT_FAILURE;
-    }
-    while(1) {
-        if(deadEnd_flag) {   // quando si attiva la deadEnd muore il main
-            printf("\nBella, se beccamo.\n");
-            break;
-        }
-    }
-    close(sockfd);
-    return EXIT_SUCCESS;
+    sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+    memset(&server_addr, '\0', sizeof(server_addr));
+
+      
 }
